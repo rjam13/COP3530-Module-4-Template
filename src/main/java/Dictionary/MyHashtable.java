25e836b0e9a3cf6a2786b626643c9913b13fb954
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2023-03-07 11:58:48 -0500

    first commit

---
+++ src/main/java/Dictionary/MyHashtable.java
@@ -0,0 +1,84 @@
+package Dictionary;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import List.MyLinkedList;
+
+class MyHashtable implements DictionaryInterface {
+
+    protected MyLinkedList[] table;
+    // Returns the size of the biggest bucket (most collisions) in the hashtable.
+    public int biggestBucket()
+    {
+        int biggestBucket = 0;
+        for(int i = 0; i < table.length; i++) {
+            // Loop through the table looking for non-null locations.
+            if (table[i] != null) {
+                // If you find a non-null location, compare the bucket size against the largest
+                // bucket size found so far. If the current bucket size is bigger, set biggestBucket
+                // to this new size.
+                MyLinkedList bucket = table[i];
+                if (biggestBucket < bucket.size())
+                    biggestBucket = bucket.size();
+            }
+        }
+        return biggestBucket; // Return the size of the biggest bucket found.
+    }
+
+    // Returns the average bucket length. Gives a sense of how many collisions are happening overall.
+    public float averageBucket() {
+        float bucketCount = 0; // Number of buckets (non-null table locations)
+        float bucketSizeSum = 0; // Sum of the size of all buckets
+        for(int i = 0; i < table.length; i++) {
+            // Loop through the table
+            if (table[i] != null) {
+                // For a non-null location, increment the bucketCount and add to the bucketSizeSum
+                MyLinkedList bucket = table[i];
+                bucketSizeSum += bucket.size();
+                bucketCount++;
+            }
+        }
+
+        // Divide bucketSizeSum by the number of buckets to get an average bucket length.
+        return bucketSizeSum/bucketCount;
+    }
+
+    public String toString()
+    {
+        String s = "";
+        for(int tableIndex = 0; tableIndex < size(); tableIndex++) {
+            if (table[tableIndex] != null) {
+                MyLinkedList bucket = table[tableIndex];
+                for(int listIndex = 0; listIndex < bucket.size(); listIndex++) {
+                    Entry e = (Entry)bucket.get(listIndex);
+                    s = s + "key: " + e.key + ", value: " + e.value + "\n";
+                }
+            }
+        }
+        return s;
+    }
+
+    protected class Entry
+    {
+        String key;
+        Object value;
+    }
+
+    // Implement these methods
+    public boolean isEmpty() {return true;} // returns true if the Dictionary is empty, false otherwise.
+
+    public int size(){return -1;} //Returns the number of key/value pairs stored in the dictionary.
+
+    // Adds a value stored under the given key. If the key has already been stored in the Dictionary,
+    // replaces the value associated with the key and returns the old value. If the key isn't in the dictionary
+    // returns null.
+    public Object put(String key, Object value){ return null;}
+
+    public Object get(String key){ return null;} // Retrieves the value stored with the key.
+
+    public void remove(String key){} // Deletes the key/value pair stored with the given key.
+
+    public void clear(){} // Empties the dictionary.
+
+    public String[] getKeys(){return null;}
+
+}
